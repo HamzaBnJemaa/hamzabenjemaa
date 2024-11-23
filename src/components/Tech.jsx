91de8d4679df05f5433c8aa04a8c295a9afd2308
HamzaBnJemaa <benjemaahamza09@gmail.com>
2024-11-23 14:42:37 +0100

    logostech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { BiLogoTypescript } from 'react-icons/bi'
+import { BiLogoDocker, BiLogoJavascript, BiLogoTypescript } from 'react-icons/bi'
 
 const Tech = () => {
   return (
     <div id='tech' className='flex min-h-[70vh] w-full flex-col items-center justify-center gap-16 md:gap-32'>
       <h1 className='text-4xl font-light text-white md:text-6xl'>Technologies</h1>
       <div className='flex flex-wrap items-center justify-center gap-10 p-5'>
-        <div classname='cursor-pointer text-[80px] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'>
+        <div>
   <BiLogoTypescript className='cursor-pointer text-[80px] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'/>
+        </div>
+        <div>
+  <BiLogoJavascript className='cursor-pointer text-[80px] text-orange-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'/>
+        </div>
+        <div>
+  <BiLogoDocker className='cursor-pointer text-[80px] text-blue-600 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'/>
         </div>
       </div>
     </div>
