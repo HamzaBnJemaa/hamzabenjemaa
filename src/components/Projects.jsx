4ae2fcc254afd3bc7db381144057b78adf160fb0
HamzaBnJemaa <benjemaahamza09@gmail.com>
2024-11-27 23:23:40 +0100

    last but not least

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import image1 from '../assets/hamza.png';
+import image1 from '../assets/ebookstore.jpg';
 import image2 from '../assets/codini.jpg';
 import image3 from '../assets/hamza4.png';
 import image4 from '../assets/hamza5.jpg';
@@ -9,15 +9,15 @@ import { motion } from "framer-motion";
 const projectsData = [
   {
     image:image1,
-    title: "Habit Trackerr",
-    description: "lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor.",
+    title: "E-Book Store",
+    description: "lorem ips lorem ipsum dorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolo lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor.",
     technologies: ["HTML", "CSS", "JavaScript","CSS", "MySQL"],
   },
   {
     image:image2,
     title: "Codini Platform",
-    description: "lorem Ipsum",
-    technologies: ["HTML", "CSS", "JavaScript","CSS", "MySQL"],
+    description: "CODINI is a digital coding school to learn how to code from the beginning with many levels & many programming languages assisted by the elite teachers ...      ",
+    technologies: ["JavaScript","TypeScript", "Next.js14" , "tailwind", "react native", "cloudinary", "sequelize", "MongoDB", "JWT"],
   },
   {
     image:image3,
@@ -31,12 +31,7 @@ const projectsData = [
     description: "lorem Ipsum",
     technologies: ["HTML", "CSS", "JavaScript","CSS", "MySQL"],
   },
-  {
-    image:image5,
-    title: "admin dashboard",
-    description: "lorem Ipsum",
-    technologies: ["HTML", "CSS", "JavaScript","CSS", "MySQL"],
-  },
+
 ]
 
 const ScrollReveal = ({children}) => {
