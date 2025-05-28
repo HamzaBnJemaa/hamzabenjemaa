185a87eb46474bb032aace353b05c85e036425b7
HamzaBnJemaa <benjemaahamza09@gmail.com>
2025-05-28 02:05:24 +0100

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,11 @@ function App() {
       <div className="fixed top-0 left-0 w-screen h-screen z-[-1]">
         <div className="w-full h-full bg-black bg-[radial-gradient(ellipse_at_center,_#111827,_#000000)]"></div>
       </div>
+{isLoading && (
+  <div className="fixed inset-0 bg-black z-50 flex items-center justify-center">
+    <span className="text-white text-lg">Loading...</span>
+  </div>
+)}
 
       {/* Main Content */}
       <>
